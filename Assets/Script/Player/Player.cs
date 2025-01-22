5b603432c17bee3ddb83f31436ab96bfab48ba76
AukarF <aukarafrank@gmail.com>
2025-01-21 21:49:19 -0400

    adding run key

--- Assets/Script/Player/Player.cs
+++ Assets/Script/Player/Player.cs
@@ -6,21 +6,57 @@ using System.Collections.Generic;
 public class Player : MonoBehaviour
 {
     public Rigidbody2D myrigidbody;
-    public Vector2 velocity;
+
+    public Vector2 friction = new Vector2(.1f, 0);
+
     public float speed;
+    public float speedRun;
+
+    public float forceJump = 2f;
+
+    private float _currentSpeed;
 
     private void Update()
     {
+        HandleJump();
+        HandleMovement();
+    }
+
+    private void HandleMovement()
+    {
+        if (Input.GetKey(KeyCode.LeftControl))
+            _currentSpeed = speedRun;
+        else
+            _currentSpeed = speed;
+
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             //myrigidbody.MovePosition(myrigidbody.position - velocity * Time.deltaTime);
-            myrigidbody.velocity = new Vector2(-speed,myrigidbody.velocity.y);
+            myrigidbody.linearVelocity = new Vector2(-_currentSpeed, myrigidbody.linearVelocity.y);
         }
 
-        else  if (Input.GetKey(KeyCode.RightArrow))
+        else if (Input.GetKey(KeyCode.RightArrow))
         {
             //myrigidbody.MovePosition(myrigidbody.position + velocity * Time.deltaTime);
-            myrigidbody.velocity = new Vector2(speed,myrigidbody.velocity.y);
+            myrigidbody.linearVelocity = new Vector2(_currentSpeed, myrigidbody.linearVelocity.y);
+        }
+
+
+        if (myrigidbody.linearVelocity.x > 0)
+        {
+            myrigidbody.linearVelocity -= friction;
+        }
+        else if (myrigidbody.linearVelocity.x < 0)
+        {
+            myrigidbody.linearVelocity += friction;
         }
     }
+
+    private void HandleJump()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+            myrigidbody.linearVelocity = Vector2.up * forceJump;
+    }
 }
+
+
