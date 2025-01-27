a37b77d272969525091e25f62db81ceb3af2a1f5
AukarF <aukarafrank@gmail.com>
2025-01-26 22:29:38 -0400

    Added Projectiles

---
+++ Assets/Script/Gun/GunBase.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GunBase : MonoBehaviour
+{
+    public ProjectileBase prefabprojectile;
+
+    public Transform positionToShoot;
+    public float timeBetweenShoot = .3f;
+    public Transform playerSideReference;
+
+    private Coroutine _currentCoroutine;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            _currentCoroutine = StartCoroutine(StartShoot());
+        }
+        else if (Input.GetKeyUp(KeyCode.S))
+        {
+            if (_currentCoroutine != null) 
+                StopCoroutine(_currentCoroutine);
+        }
+    }
+
+    IEnumerator StartShoot()
+    {
+        while(true)
+        {
+            Shoot();
+            yield return new WaitForSeconds(timeBetweenShoot);
+        }
+    }
+
+    public void Shoot()
+    {
+        var projectile = Instantiate(prefabprojectile);
+        projectile.transform.position = positionToShoot.position;
+        projectile.side = playerSideReference.transform.localScale.x;
+    }
+}
