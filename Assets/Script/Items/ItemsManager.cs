c29c0aafab54d61277a3f985645fe36f550cb2eb
AukarF <aukarafrank@gmail.com>
2025-01-27 18:49:15 -0400

    Uptade Ui classic mode

--- Assets/Script/Items/ItemsManager.cs
+++ Assets/Script/Items/ItemsManager.cs
@@ -2,12 +2,13 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using Ebac.Core.Singleton;
+using TMPro;
 
 public class ItemsManager : Singleton<ItemsManager>
 {
 
     public int coins;
-
+    public TextMeshProUGUI uiTextCoins;
 
     private void Start()
     {
@@ -18,10 +19,18 @@ public class ItemsManager : Singleton<ItemsManager>
     private void Reset()
     {
         coins = 0;
+        UptadeUI();
     }
 
     public void AddCoins(int amount = 1)
     {
         coins += amount;
+        UptadeUI();
     }
+
+    private void UptadeUI()
+    {
+        uiTextCoins.text = coins.ToString();
+    }
+
 }
